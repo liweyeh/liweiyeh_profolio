5224497dfdd02a8fab29f6b7df3653522f4fe334
Liwei Yeh <l.yeh@uqconnect.edu.au>
2020-03-29 14:56:27 +1000

    reference work

--- client/src/components/contact/Contact.js
+++ client/src/components/contact/Contact.js
@@ -60,6 +60,25 @@ const Contact = () => {
             defaultMsg='Contact Me'
           />
         </Grid>
+        <Grid item>
+          <div>
+            {`Icons made by `}
+            <a
+              href='https://www.flaticon.com/authors/pixel-perfect'
+              title='Pixel perfect'
+            >
+              {`Pixel perfect`}
+            </a>
+            {` & `}
+            <a href='https://www.flaticon.com/authors/freepik' title='Freepik'>
+              Freepik
+            </a>
+            {` from `}
+            <a href='https://www.flaticon.com/' title='Flaticon'>
+              www.flaticon.com
+            </a>
+          </div>
+        </Grid>
       </Grid>
     </div>
   );
